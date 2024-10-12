c8b765468fe2980a00f3e8efe7732e600ddca4b1
Shion Sonozaki <sssyaon@gmail.com>
2024-10-12 18:13:55 +0700

    management service: send msg to rabbit without needless field

--- management-service-nodejs/src/modules/videos/videos.service.ts
+++ management-service-nodejs/src/modules/videos/videos.service.ts
@@ -2,13 +2,15 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CreateVideoDto } from './dto/create-video.dto';
 import { ClientProxy } from '@nestjs/microservices';
 import { VideoInfoDto } from './dto/video-info.dto';
+import { omit } from 'lodash';
 
 @Injectable()
 export class VideosService {
   @Inject('DOWNLOAD_QUEUE') private readonly downloadClient: ClientProxy;
 
   download(createDto: CreateVideoDto) {
-    this.downloadClient.emit('video_to_download_queue', createDto);
+    const fields = omit(createDto, ['folderID']);
+    this.downloadClient.emit('video_to_download_queue', fields);
   }
 
   saveNewVideo(data: VideoInfoDto) {
